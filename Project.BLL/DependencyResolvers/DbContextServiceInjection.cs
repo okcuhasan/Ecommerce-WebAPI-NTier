16fe29b3a8aaf05c8076b1312ebde910a9bc73c1
Hasan Okcu <okcuhhasan@gmail.com>
2024-03-26 12:31:37 +0300

    dependency resolver

---
+++ Project.BLL/DependencyResolvers/DbContextServiceInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Project.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DependencyResolvers
+{
+    public static class DbContextServiceInjection
+    {
+        public static IServiceCollection AddDbContextService(this IServiceCollection services)
+        {
+            ServiceProvider provider = services.BuildServiceProvider(); // builder service ler için sağlayıcı(provider) oluştur
+            IConfiguration configuration = provider.GetService<IConfiguration>(); // builder service sağlayıcısı olan provider GetService metodu ile IConfiguration u çağırıyor
+            services.AddDbContext<MyContext>(options => options.UseSqlServer(configuration.GetConnectionString("MyConnection")).UseLazyLoadingProxies());
+            return services;
+        }
+    }
+}
---
+++ Project.BLL/DependencyResolvers/RepositoryManagerServiceInjection.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Project.BLL.ManagerServices.Abstracts;
+using Project.BLL.ManagerServices.Concretes;
+using Project.DAL.Repositories.Abstracts;
+using Project.DAL.Repositories.Concretes;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DependencyResolvers
+{
+    public static class RepositoryManagerServiceInjection
+    {
+        public static IServiceCollection AddRepositoryManagerServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>)); // generic ifade ne olursa olsun typeof ile instance ını alıyor olacağım
+            services.AddScoped(typeof(IManager<>), typeof(BaseManager<>)); // generic ifade ne olursa olsun typeof ile instance ını alıyor olacağım    
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductManager, ProductManager>();
+            services.AddScoped<IAppUserRepository, AppUserRepository>();
+            services.AddScoped<IAppUserManager, AppUserManager>();
+            services.AddScoped<IAppUserProfileRepository, AppUserProfileRepository>();
+            services.AddScoped<IAppUserProfileManager, AppUserProfileManager>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICategoryManager, CategoryManager>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderManager, OrderManager>();
+            services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+            services.AddScoped<IOrderDetailManager, OrderDetailManager>();
+
+            return services;
+        }
+    }
+}
